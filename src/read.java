6b78e0642c10b7548ba90af131771cdfdd1163c8
Noah Willaime <noah.willaime9@etu.univ-lorraine.fr>
2019-03-17 17:40:24 +0100

    Crop image

--- src/read.java
+++ src/read.java
@@ -113,6 +113,7 @@ public class read {
             BufferedReader buff = new BufferedReader(lecture);
             String line;
             int k = 0;
+            int y = 0;
             int col = 0;
             File fcrop= new File("crop_"+name);
             fcrop.createNewFile();
@@ -125,10 +126,21 @@ public class read {
             int hauteur= Integer.parseInt(sizes[1]);
             fc.write(longueur/2+" "+hauteur/2+"\n");
             fc.write(buff.readLine()+"\n");
-            int[][] tab = new int[3][longueur*hauteur];
+            int[][] tabR = new int[hauteur][longueur];
+            int[][] tabG = new int[hauteur][longueur];
+            int[][] tabB = new int[hauteur][longueur];
             while ((line = buff.readLine()) != null){
+                if (k >= longueur){
+                    k = 0;
+                    y++;
+                }
                 if (col < 3){
-                    tab[col][k] = Integer.parseInt(line);
+                    if (col == 0)
+                        tabR[y][k] = Integer.parseInt(line);
+                    else if (col == 1)
+                        tabG[y][k] = Integer.parseInt(line);
+                    else if (col == 2)
+                        tabB[y][k] = Integer.parseInt(line);
                 }
                 col++;
                 if (col == 3) {
@@ -137,33 +149,31 @@ public class read {
                 }
             }
             int mR = 0;
-            int mG = 0;
-            int mB = 0;
-            int i = 0;
+            int mG;
+            int mB;
+            int i;
             int j = 0;
-            while (j < hauteur) {
-                while (i < longueur) {
-                    mR += tab[0][j + i*longueur];
-                    mR += tab[0][j+(i + 1)*longueur];
-                    mR += tab[0][(j+1)+i*longueur];
-                    mR += tab[0][(j+1)+(i+1)*longueur];
-//
-                    mG += tab[1][j + i*longueur];
-                    mG += tab[1][j+(i + 1)*longueur];
-                    mG += tab[1][(j+1)+i*longueur];
-                    mG += tab[1][(j+1)+(i+1)*longueur];
+            while (j+1 < hauteur) {
+                i = 0;
+                while (i+1 < longueur) {
+                    mR = tabR[j][i];
+                    mR += tabR[j][i+1];
+                    mR += tabR[j+1][i];
+                    mR += tabR[j+1][i+1];
+
+                    mG = tabG[j][i];
+                    mG += tabG[j][i+1];
+                    mG += tabG[j+1][i];
+                    mG += tabG[j+1][i+1];
+
+                    mB = tabB[j][i];
+                    mB += tabB[j][i+1];
+                    mB += tabB[j+1][i];
+                    mB += tabB[j+1][i+1];
                     //
-                    mB += tab[2][j + i*longueur];
-                    mB += tab[2][j+(i + 1)*longueur];
-                    mB += tab[2][(j+1)+i*longueur];
-                    mB += tab[2][(j+1)+(i+1)*longueur];
-                    //
-                    fc.write(mR / 4 + "\n");
-                    fc.write(mG / 4 + "\n");
-                    fc.write(mB / 4 + "\n");
-                    mR = 0;
-                    mG = 0;
-                    mB = 0;
+                    fc.write(mR/4+"\n");
+                    fc.write(mG/4+"\n");
+                    fc.write(mB/4+"\n");
                     i += 2;
                 }
                 j += 2;
